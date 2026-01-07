ad22038291fbe5af7d2700fbba1f3403dda4e071
yuanyang <yoy_87@163.com>
2026-01-07 18:10:54 +0900

    Add files via upload

--- utils/tools.py
+++ utils/tools.py
@@ -1,5 +1,24 @@
 import uuid
+from typing import List, Any
 
 
 def make_id(prefix: str):
+    """create id"""
     return f"{prefix}_{uuid.uuid4().hex[:8]}"
+
+
+def append_reducer(old_list: List[Any], new_items: Any) -> List[Any]:
+    """list 方法"""
+    if old_list is None:
+        old_list = []
+    if new_items is None:
+        return old_list
+    # 如果新内容本身是列表，则扩展
+    if isinstance(new_items, list):
+        if old_list:
+            return old_list + new_items
+        return new_items
+    # 否则作为单个元素追加
+    if old_list:
+        return old_list + [new_items]
+    return [new_items]
